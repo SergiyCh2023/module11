0c7f57229d16a2788694a6b4ac106c02d32dd0f1
SergiyCh2023 <alien13579@ukr.net>
2023-03-13 19:08:03 +0200

    My Homework#11
    Was created this metods:
    1. String createStringNamesMethod(List<String> listNames)
    2.List<String> upperCaseSortMethod(List<String> list)
    3. List<String>  unitedSortedNumbers (List<String> list)
    4. Stream<Long> infinityLongStream(Long seed, Long a, Long c, Long m)
    5.<T> Stream<T> zip(Stream<T> first, Stream<T> second)

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static String createStringNamesMethod(List<String> listNames) {
+        String result = "";
+        List<String> listResult = listNames.stream()
+                .filter(e -> listNames.indexOf(e) % 2 != 0)
+                .map(e -> new String(listNames.indexOf(e) + "." + e))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < listResult.size(); i++) {
+            if (i != listResult.size() - 1) result += listResult.get(i) + " ";
+            else result += listResult.get(i);
+        }
+        return result;
+    }
+
+
+
+    public static List<String> upperCaseSortMethod(List<String> list) {
+        List<String> result = list.stream()
+                .map(s -> s.toUpperCase())
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        return result;
+    }
+
+
+
+    public static List<String>  unitedSortedNumbers (List<String> list) {
+        List<String> result = list.stream()
+                .map(s -> s.split("\\,"))
+                .flatMap(Arrays::stream)
+                .map(s -> s.trim())
+                .sorted()
+                .collect(Collectors.toList());
+        return result;
+    }
+
+
+
+    public static Stream<Long> infinityLongStream(Long seed, Long a, Long c, Long m) {
+        return Stream.iterate(seed, value->(a*value+c)%m);
+    }
+
+
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        List <T> firstList = first.collect(Collectors.toList());
+        List <T> secondList = second.collect(Collectors.toList());
+        List<T> united = new ArrayList<>();
+        int counter1=0;
+        int counter2=0;
+        int counter3=0;
+        while (firstList.size()>counter2 && secondList.size()>counter3){
+            if (counter1%2 == 0) united.add(counter1++, firstList.get(counter2++));
+            else united.add(counter1++, secondList.get(counter3++));
+        }
+        return united.stream();
+    }
+
+
+}
